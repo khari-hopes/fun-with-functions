2044bd972225355113fa885cd6f9a297e2284449
Khari Hopes <buildingkhari@yahoo.com>
2023-08-17 20:13:22 -0500

    problem 6 is passing

--- js/app.js
+++ js/app.js
@@ -149,10 +149,17 @@ let testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
   // Write your code here
+  function multiply(a, b) {
+    return a * b;
+  }
 
+  let product = dynamicArray.reduce(multiply, 1);
+  let str = `The numbers ${dynamicArray.join(',')} have a product of ${product}.`;
+
+  return [product, str];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
